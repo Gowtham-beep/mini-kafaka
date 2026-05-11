6325358bf972f8b17e48b35fe4080475f5fa9845
Gowtham-beep <gn86923@gmail.com>
2026-05-11 16:33:03 +0530

    feat: reorganize constants in Segment class for better readability and maintainability

--- src/main/java/com/minibroker/log/Segment.java
+++ src/main/java/com/minibroker/log/Segment.java
@@ -10,6 +10,14 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.zip.CRC32;
 
 public class Segment {
+
+    private static final long MAX_FILE_SIZE = 1024 * 1024 * 1024; 
+    private static final int INDEX_INTERVAL = 4096;
+    private static final int INDEX_ENTRY_SIZE = 16;
+
+
+    private static final long MAX_INDEX_SIZE = (MAX_FILE_SIZE / INDEX_INTERVAL) * INDEX_ENTRY_SIZE;
+    
    
     private final MappedByteBuffer logBuffer;
     private final MappedByteBuffer indexBuffer;
@@ -18,16 +26,13 @@ public class Segment {
     private final AtomicLong messageCount = new AtomicLong(0);
     private final AtomicLong indexPosition = new AtomicLong(0);
     private final AtomicLong lastIndexBytesPos = new AtomicLong(0);
-    private static final int INDEX_INTERVAL = 4096;
-    private static final int INDEX_ENTRY_SIZE = 16;
+   
+
 
     private final FileChannel logFileChannel;
     private final FileChannel indexFileChannel;
     private volatile boolean isSealed = false;
-    private static final long MAX_FILE_SIZE = 1024 * 1024 * 1024; 
-
-    private static final long MAX_INDEX_SIZE = (MAX_FILE_SIZE / INDEX_INTERVAL) * INDEX_ENTRY_SIZE;
-    
+   
     public Segment(long baseOffset, Path logPath, Path indexPath) throws  IOException{
         this.baseOffset = baseOffset;
 
@@ -97,9 +102,7 @@ public class Segment {
         while(true){
             long currentLast = this.lastIndexBytesPos.get();
             if((claimedWritePos - currentLast)>=INDEX_INTERVAL){
-
                 if(this.lastIndexBytesPos.compareAndSet(currentLast, claimedWritePos)){
-
                     writeIndexEntry(claimedLogicalOffset,claimedWritePos);
                     break;
                 }
@@ -131,8 +134,9 @@ public class Segment {
         if(logicalOffset<this.baseOffset){
             throw new OffsetOutOfRangeException("Invalid logical offset: " + logicalOffset);
         }
-        long maxOffset = this.baseOffset + this.messageCount.get();
 
+
+        long maxOffset = this.baseOffset + this.messageCount.get();
         if(logicalOffset >= maxOffset){
             throw new OffsetOutOfRangeException("Offset not yet written: " + logicalOffset);
         }
@@ -193,5 +197,21 @@ public class Segment {
         }
 
         return payload;
+    }
+
+    public long getBaseOffset() {
+        return baseOffset;
+    }
+
+    public long getMessageCount() {
+        return messageCount.get();
+    }
+
+    public boolean isSealed() {
+        return isSealed;
+    }
+
+    public long getWritePosition() {
+        return writePosition.get();
     }
 }
