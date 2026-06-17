b1f1cd7daaa500d1b95635bd3fa4ea65795ec79f
Gowtham-beep <gn86923@gmail.com>
2026-06-17 13:43:37 +0530

    feat: update RaftRpcEncoder to improve message encoding and handle edge cases

--- src/main/java/com/minibroker/raft/rpc/RaftRpcEncoder.java
+++ src/main/java/com/minibroker/raft/rpc/RaftRpcEncoder.java
@@ -12,10 +12,11 @@ public class RaftRpcEncoder extends MessageToByteEncoder<RaftMessage> {
     @Override
     protected void encode(ChannelHandlerContext ctx,RaftMessage msg,ByteBuf out) throws Exception{
         switch(msg){
-            AppendEntriesRequest req -> encodeAppendEntriesRequest(req,out);
-            AppendEntrieResponse res -> encodeAppendEntrieResponse(res,out);
-            RequestVoteRequest req -> encodeRequestVoteRequest(req,out);
-            RequestVoteResponse res -> encodeRequestVoteResponse(res,out);
+            case AppendEntriesRequest req -> encodeAppendEntriesRequest(req,out);
+            case AppendEntrieResponse res -> encodeAppendEntrieResponse(res,out);
+            case RequestVoteRequest req -> encodeRequestVoteRequest(req,out);
+            case RequestVoteResponse res -> encodeRequestVoteResponse(res,out);
+            default -> throw new IllegalArgumentException("Unknown message type: " + msg);
         }
         
     }
@@ -32,17 +33,39 @@ public class RaftRpcEncoder extends MessageToByteEncoder<RaftMessage> {
         out.writeLong(req.prevLogIndex());
         out.writeLong(req.prevLogTerm());
 
-        if(req.entries()!=null && req.entries().isEmpty()){
+        if(req.entries()==null || req.entries().isEmpty()){
             out.writeInt(0);
         }else{
             out.writeInt(req.entries().size());
             for(LogEntry entry:req.entries()){
                 out.writeLong(entry.term());
-                out.writeLong(entry.payload().length);
+                out.writeInt(entry.payload().length);
                 out.writeBytes(entry.payload());
             }
         }
         out.writeLong(req.leaderCommit());
+    }
 
+    private void encodeAppendEntrieResponse(AppendEntrieResponse res, ByteBuf out){
+        out.writeByte(2);
+        out.writeLong(res.term());
+        out.writeByte(res.success()?1:0);
+    }
+
+    private void encodeRequestVoteRequest(RequestVoteRequest req, ByteBuf out){
+        out.writeByte(3);
+        out.writeLong(req.term());
+        String candidateId = req.candidateId()!=null?req.candidateId():"";
+        int candidateIdLen = ByteBufUtil.utf8Bytes(candidateId);
+        out.writeInt(candidateIdLen);
+        out.writeCharSequence(candidateId, StandardCharsets.UTF_8);
+        out.writeLong(req.lastLogIndex());
+        out.writeLong(req.lastLogTerm());
+    }
+
+    private void encodeRequestVoteResponse(RequestVoteResponse res, ByteBuf out) {
+        out.writeByte(4);
+        out.writeLong(res.term());
+        out.writeByte(res.voteGranted() ? 1 : 0);
     }
 }
