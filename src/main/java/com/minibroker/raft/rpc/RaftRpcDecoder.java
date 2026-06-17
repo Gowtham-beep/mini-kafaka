6b1577b650994d5a0506633de69a8686b4963569
Gowtham-beep <gn86923@gmail.com>
2026-06-17 16:24:52 +0530

    feat: change RaftRpcDecoder to extend MessageToMessageDecoder for improved message handling

--- src/main/java/com/minibroker/raft/rpc/RaftRpcDecoder.java
+++ src/main/java/com/minibroker/raft/rpc/RaftRpcDecoder.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageDecoder;
 
-public class RaftRpcDecoder extends ByteToMessageDecoder {
+public class RaftRpcDecoder extends MessageToMessageDecoder<ByteBuf> {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
