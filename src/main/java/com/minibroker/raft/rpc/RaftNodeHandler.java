dfc0cb39bcfbe0466bd3624510bcb4707132f135
Gowtham-beep <gn86923@gmail.com>
2026-06-18 12:56:00 +0530

    feat: update RaftNodeHandler and RaftServer to support executor for state machine processing and improve server shutdown handling

--- src/main/java/com/minibroker/raft/rpc/RaftNodeHandler.java
+++ src/main/java/com/minibroker/raft/rpc/RaftNodeHandler.java
@@ -1,34 +1,45 @@
 package com.minibroker.raft.rpc;
 
+import java.util.concurrent.Executor;
+
 import com.minibroker.raft.RaftNode;
 
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import io.netty.util.concurrent.EventExecutorGroup;
+
 public class RaftNodeHandler extends SimpleChannelInboundHandler<RaftMessage>{
     private final RaftNode raftNode;
+    private final Executor stateMachineExecuter;
 
-    public RaftNodeHandler(RaftNode raftNode){
+    public RaftNodeHandler(RaftNode raftNode, Executor stateMachineExecuter){
         this.raftNode = raftNode;
+        this.stateMachineExecuter = stateMachineExecuter;
     }
 
     @Override
     protected void channelRead0( ChannelHandlerContext ctx, RaftMessage msg){
-        switch(msg){
-            case AppendEntriesRequest req ->{
-                AppendEntrieResponse response = raftNode.handleAppendEntriesRequest(req);
-                ctx.writeAndFlush(response).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+        stateMachineExecuter.execute(() -> {
+            try{
+                switch(msg){
+                case AppendEntriesRequest req ->{
+                    AppendEntrieResponse response = raftNode.handleAppendEntriesRequest(req);
+                    ctx.writeAndFlush(response).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+                }
+                case RequestVoteRequest req ->{
+                    RequestVoteResponse response = raftNode.handleRequestVote(req);
+                    ctx.writeAndFlush(response).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+                }
+                default->{
+                    System.err.println("Received unexpected message type in Server pipeline: " + msg.getClass().getSimpleName());
+                }
             }
-            case RequestVoteRequest req ->{
-                RequestVoteResponse response = raftNode.handleRequestVote(req);
-                ctx.writeAndFlush(response).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+            }catch(Exception e){
+                ctx.fireExceptionCaught(e);
             }
-            default->{
-                System.err.println("Received unexpected message type in Server pipeline: " + msg.getClass().getSimpleName());
-            }
-        }
-
+        });
     }
 
     @Override
--- src/main/java/com/minibroker/raft/rpc/RaftServer.java
+++ src/main/java/com/minibroker/raft/rpc/RaftServer.java
@@ -1,24 +1,33 @@
 package com.minibroker.raft.rpc;
 
-import java.nio.channels.Channel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import com.minibroker.raft.RaftNode;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.util.concurrent.DefaultEventExecutor;
-import io.netty.util.concurrent.DefaultEventExecutorGroup;
-import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
 
 public class RaftServer {
     private final int port;
     private final RaftNode raftNode;
 
-    private EventLoopGroup boassGroup;
+    private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
 
-    private EventExecutorGroup rafEventExecutorGroup;
+    private ExecutorService raftExecutorService;
     private Channel serverChannel;
 
     public RaftServer(int port ,RaftNode raftNode){
@@ -27,12 +36,63 @@ public class RaftServer {
     }
 
     public void start() throws Exception{
-        boassGroup = new NioEventLoopGroup(1);
-        workerGroup = new NioEventLoopGroup();
-        rafEventExecutorGroup = new DefaultEventExecutorGroup();
+        bossGroup = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
+        workerGroup = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
+        raftExecutorService = Executors.newFixedThreadPool(4, runnable -> {
+            Thread thread = new Thread(runnable);
+            thread.setName("raft-state-worker-" + thread.threadId());
+            thread.setDaemon(true);
+            return thread;
+        });
 
         ServerBootstrap bootstrap = new ServerBootstrap();
-        bootstrap.group(boassGroup,workerGroup);
-            .cha
+        bootstrap.group(bossGroup,workerGroup)
+            .channel(NioServerSocketChannel.class)
+            .option(ChannelOption.SO_REUSEADDR,true)
+            .childOption(ChannelOption.SO_KEEPALIVE,true)
+            .childOption(ChannelOption.TCP_NODELAY,true)
+            .childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch){
+                    ChannelPipeline pipeline = ch.pipeline();
+
+                    pipeline.addLast("encoder", new RaftRpcEncoder());
+                    
+                    pipeline.addLast("Framedecoder", new LengthFieldBasedFrameDecoder(
+                        10 * 1024 * 1024, 0, 4, 0, 4));
+                    pipeline.addLast("decoder", new RaftRpcDecoder());
+                    pipeline.addLast("raftHandler", new RaftNodeHandler(raftNode, raftExecutorService));
+                } 
+            });
+            ChannelFuture future = bootstrap.bind(port).sync();
+            this.serverChannel = future.channel();
+            System.out.printf("Raft Server Node successfully bound and listening on TCP port %d%n", port);
+    }
+
+    public void shutDown(){
+        System.out.printf("Initiating graceful shutdown sequence for Raft Server on port %d...%n", port);
+        try{
+            if(serverChannel!=null){
+                serverChannel.close().sync();
+            }
+        }catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }finally{
+            if (raftExecutorService != null) {
+                raftExecutorService.shutdown();
+                try {
+                    if (!raftExecutorService.awaitTermination(5, TimeUnit.SECONDS)) {
+                        raftExecutorService.shutdownNow();
+                    }
+                } catch (InterruptedException e) {
+                    raftExecutorService.shutdownNow();
+                    Thread.currentThread().interrupt();
+                }
+            }
+            if(workerGroup!=null) workerGroup.shutdownGracefully();
+            if(bossGroup!=null) bossGroup.shutdownGracefully();
+
+            System.out.printf("Raft Server on port %d successfully halted.%n", port);
+        }
     }
 }
